a75888cb39c4fb62d7f2b1def9a19a3e7b407fce
liuwenjin <liuwenjin@songshuai.com>
2021-09-09 16:54:29 +0800

    业务重构

--- src/main/java/com/classba/persistent/mysql.java
+++ src/main/java/com/classba/persistent/mysql.java
@@ -1,11 +1,29 @@
 package com.classba.persistent;
 
-import java.io.IOException;
+import redis.clients.jedis.Jedis;
 
 public class mysql
 {
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
+		Jedis jedis = new Jedis();
+		jedis.set("num", "1");
+		new Demo().start();
+		new Demo().start();
+	}
+}
 
+
+class Demo extends Thread
+{
+	public void run()
+	{
+		Jedis jedis1 = new Jedis();
+		for (int i = 0; i < 100; i++) {
+			int num = Integer.parseInt(jedis1.get("num"));
+			num = num + 1;
+			jedis1.set("num", num + "");
+			System.out.println(jedis1.get("num"));
+		}
 	}
 }
